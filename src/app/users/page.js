7e0d0992075c96fdad119aa85d50edcfcd7bd853
Hamza Hussaini <hamza@Hamzas-MacBook-Air.local>
2024-01-25 11:19:26 -0500

    added basic nextjs setup

---
+++ src/app/users/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UsersPage = async () => {
+
+    const res = await fetch('http://localhost:3000/');
+    const value = await res.json();
+
+    console.log("value here ", value);
+
+    return (
+        <div>UsersPage</div>
+    )
+
+}
+
+export default UsersPage
