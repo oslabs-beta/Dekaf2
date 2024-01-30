cdded4174a34ba6f57dc8c9c70a373831fe8479c
Diogo de Souza <hello@diogodesouza.com>
2024-01-30 16:59:21 -0500

    comitting to merge

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,14 +1,19 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Kafka App",
+export const metadata: Metadata = {
+  title: "Dekaf",
   description: "Kafka Error Handling Tool",
 };
 // children is replaced by a page dynamically at runtime depending on where the user is in our application
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- companionApp/consumer.js
+++ companionApp/consumer.js
@@ -1,35 +1,79 @@
 const kafka = require('./kafka')
 
-const consumer = kafka.consumer({
-  groupId: 'test_group'
-})
+class Consumer {
 
-const main = async () => {
-  await consumer.connect()
+  constructor(groupId){
+    this.groupId = groupId;
+    this.consumer = kafka.consumer({
+      groupId: this.groupId
+    })
+  }
 
-  await consumer.subscribe({
-    topic: 'topic_1',
-    fromBeginning: true
-  })
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log('Received message', {
-        topic,
-        partition,
-        key: message.key.toString(),
-        value: message.value.toString()
+  async connect(){
+    try {
+      await this.consumer.connect();
+    }
+    catch (error) {
+      console.error('Error connecting to consumer', error);
+      throw error;
+    }
+  } 
+
+  async subscribe(topic){
+
+    try {
+      await this.consumer.subscribe({
+        //topic: 'topic_1',
+        topic: topic,
+        fromBeginning: true
       })
     }
-  })
+    catch (error) {
+      console.error('Error subscribing to consumer', error);
+      throw error;
+    }
+
+  }
+  
+  async run(){
+
+    try {
+      await this.consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          console.log('Received message', {
+            topic,
+            partition,
+            key: message.key.toString(),
+            value: message.value.toString()
+          })
+        }
+      })
+    }
+    catch (error) {
+      console.error('Error consuming messages', error);
+      throw error;
+    }
+
+  }
+    
+
 }
 
-main().catch(async error => {
-  console.error(error)
-  try {
-    await consumer.disconnect()
-  } catch (e) {
-    console.error('Failed to gracefully disconnect consumer', e)
-  }
-  process.exit(1)
-})
+module.exports = Consumer;
+
+// const consumer = new Consumer('test_group_1');
+// consumer.connect();
+// consumer.subscribe('topic_1');
+// consumer.run();
+
+// main().catch(async error => {
+//   console.error(error)
+//   try {
+//     await consumer.disconnect()
+//   } catch (e) {
+//     console.error('Failed to gracefully disconnect consumer', e)
+//   }
+//   process.exit(1)
+// })
+
