4bc268625538b746e073ca4d0a282ec9477b2cb4
Diogo de Souza <hello@diogodesouza.com>
2024-01-30 17:08:59 -0500

    deleted function running on application startup

--- server/shared/requests/ConfluentAPI.ts
+++ server/shared/requests/ConfluentAPI.ts
@@ -55,23 +55,4 @@ class ConfluentAPI implements IConfluentAPI {
   // listMessagesFromTopic(topicID: any): Promise<IMessages[]> {}
 }
 
-// curl --request GET \
-//   --url '' \
-//   --header 'Authorization: Basic REPLACE_BASIC_AUTH'
-
-// key: SWCSTOYHBUMSZQHV
-// secret: OoizmaXAe4t/G93Ph0UP2cx64zm9VdCR5ArRRWS50VUz1NbI8PVEIU0nphRvDA1q
-//encoded base64: U1dDU1RPWUhCVU1TWlFIVjpPb2l6bWFYQWU0dC9HOTNQaDBVUDJjeDY0em05VmRDUjVBclJSV1M1MFZVejFOYkk4UFZFSVUwbnBoUnZEQTFx
-// $ echo -n "SWCSTOYHBUMSZQHV:OoizmaXAe4t/G93Ph0UP2cx64zm9VdCR5ArRRWS50VUz1NbI8PVEIU0nphRvDA1q" | base64
-
-let confluentEnv = "env-3njrwm";
-let authToken =
-  "U1dDU1RPWUhCVU1TWlFIVjpPb2l6bWFYQWU0dC9HOTNQaDBVUDJjeDY0em05VmRDUjVBclJSV1M1MFZVejFOYkk4UFZFSVUwbnBoUnZEQTFx";
-
-const confluentAPI = new ConfluentAPI(authToken);
-console.log(confluentAPI.listEnvironments());
-
 module.exports = ConfluentAPI;
-
-// key: O7S5WJWB3FQSXZ6W
-// secret: zX+EjKqYybuisKY6C1rr53H2dPsY9SY3nwZkA25BRy58kiCgd/wcJ2/WF9fEcAmH
