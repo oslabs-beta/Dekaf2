bb88fd7a349be702d9a48abbee877028d323543f
Wu-Cheney <maxiwu123@gmail.com>
2024-02-09 11:09:19 -0500

    added login page

--- client/src/app/components/LoginContainer.tsx
+++ client/src/app/components/LoginContainer.tsx
@@ -1,5 +1,13 @@
 'use client'; //nextjs compiler automatically places this component into javascript bundle
 import React from 'react';
+import { signInWithGoogle } from '../login/firebase'
+import { redirect } from 'next/navigation';
+// import { useRouter } from 'next/router';
+
+// Inside your component or function
+// const { push } = useRouter();
+
+// Redirect to '/users'
 
 
 //import styles from './ProductCard.module.css';
@@ -7,10 +15,17 @@ import React from 'react';
 
 const LoginContainer = () => {
 
+    const loginHandler = () => {
+        const user =  signInWithGoogle();
+        console.log('this is user', user)
+        // redirect('/users')
+        // push('/users');
+    }
+
     return(
         <div className='shadow-lg h-full  flex flex-col items-center  w-1/3 z-10 absolute right-0 bg-white '>
            <div className='text-black text-3xl font-bold mt-64 mb-10'>Welcome to DeKaf</div>
-           <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Sign in with GitHub</button>
+           <button onClick={loginHandler} className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Sign in with Google</button>
         </div>
        
     );
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import Image from 'next/image'
 import LoginContainer from '../components/LoginContainer';
 import VideoBackground from '../components/VideoBackground';
@@ -15,10 +15,10 @@ const LoginPage = () => {
         <div className='overflow-hidden'> 
             <Image
               className='absolute'
-              src='/login-background-static.png'
+              src='/login-background2.jpg'
             //   width={500}
             //   height={500}
-              alt="Picture of the author"
+              alt="Cannot load Image"
               fill={true}
               sizes='100vw'
             //   style={styles.backgroundImg}
@@ -33,20 +33,20 @@ const LoginPage = () => {
 }
 
 export default LoginPage
-'use client'; //nextjs compiler automatically places this component into javascript bundle
-import Link from 'next/link';
-import React from 'react';
-//import styles from './ProductCard.module.css';
+// 'use client'; //nextjs compiler automatically places this component into javascript bundle
+// import Link from 'next/link';
+// import React from 'react';
+// //import styles from './ProductCard.module.css';
 
-const Login = () => {
+// const Login = () => {
 
-    return (
-        <div className='p-5 my-5 bg-sky-400 text-white'> 
-            <button onClick={ () => console.log('Clicked')}>LoginPage</button>
-            <Link href='/users' className='text-black'> Users </Link>
-        </div>
-    )
+//     return (
+//         <div className='p-5 my-5 bg-sky-400 text-white'> 
+//             <button onClick={ () => console.log('Clicked')}>LoginPage</button>
+//             <Link href='/users' className='text-black'> Users </Link>
+//         </div>
+//     )
 
-}
+// }
 
-export default Login
+// export default Login
