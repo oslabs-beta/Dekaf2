cddd285bc16f0a8962a8502b2a3673e54d581df3
Wu-Cheney <maxiwu123@gmail.com>
2024-02-13 16:48:23 -0500

    formatted navbar

--- client/src/app/components/MainChart.tsx
+++ client/src/app/components/MainChart.tsx
@@ -5,11 +5,11 @@ import styles from './MainChart.module.css'
 const MainChart = () => {
     const chartData = {
        
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'],
   datasets: [
     {
       label: 'Sales',
-      data: [65, 59, 80, 81, 56, 55, 40],
+      data: [65, 59, 80, 81, 56, 55, 40, 77],
       fill: true,
       borderColor: 'rgb(75, 192, 192)',
       tension: 0.1
