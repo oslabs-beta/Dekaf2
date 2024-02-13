e231a7255955713a695ddd70fc2d23fc1ae82901
Diogo de Souza <diogodesouza@Diogos-MacBook-Pro.local>
2024-02-13 16:30:54 -0500

    creates environmentsSlice, implements cors and merges develop branch into feature branch

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,18 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
-// ...
-import counterReducer from '../app/slices/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+
+import environmentsReducer from "../app/slices/environmentsSlice";
+import counterReducer from "../app/slices/counterSlice";
 
 //Importing method
 export const store = configureStore({
   reducer: {
+    environments: environmentsReducer,
     counter: counterReducer,
     // posts: postsReducer,
     // comments: commentsReducer,
     // users: usersReducer
-  }
-})
+  },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
