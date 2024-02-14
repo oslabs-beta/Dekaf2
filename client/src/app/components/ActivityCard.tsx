4592a32d766693d8e798cca211e5879956ba56fe
Wu-Cheney <maxiwu123@gmail.com>
2024-02-14 15:50:26 -0500

    styled header and metrics cards

--- client/src/app/components/ActivityCard.tsx
+++ client/src/app/components/ActivityCard.tsx
@@ -3,13 +3,13 @@ import React from "react"
 import styles from './ActivityCard.module.css'
 
 interface cardData {
-    cardData: string[];
+    cardData: any;
   }
 
 const ActivityCard = ({ cardData }: cardData) => {
     const elements = [];
     for (let i = 0; i < cardData.length; i++){
-       elements.push(<li className={styles.listItem}>{cardData[i]}</li>)
+       elements.push(<li className={styles.listItem}>{cardData[i].display_name} </li>)
     }
 
     return (
