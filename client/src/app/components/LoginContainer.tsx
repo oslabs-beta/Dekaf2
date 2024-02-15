4109fe36a4fd9d4654df258c33378cb3b07e8546
Wu-Cheney <maxiwu123@gmail.com>
2024-02-15 17:16:31 -0500

    added user photo and image for login

--- client/src/app/components/LoginContainer.tsx
+++ client/src/app/components/LoginContainer.tsx
@@ -20,13 +20,13 @@ const LoginContainer = () => {
   };
 
   return (
-    <div className="shadow-lg h-full  flex flex-col items-center  w-1/3 z-10 absolute right-0 bg-white ">
-      <div className="text-black text-3xl font-bold mt-64 mb-10">
+    <div className="pb-40 shadow-lg h-full  flex flex-col items-center justify-center w-1/3 z-10 absolute right-0 bg-white ">
+      <div className="text-black text-4xl font-bold  mb-10">
         Welcome to DeKaf
       </div>
       <button
         onClick={loginHandler}
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        className=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded"
       >
         Sign in with Github
       </button>
